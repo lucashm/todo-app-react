24fea41a67978169fe1d54ba42f324bb40f7caf0
Lucas Malta <lucas50xd@hotmail.com>
2018-09-06 09:15:19 -0300

    adds basic front

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, KeyboardAvoidingView } from 'react-native';
+import TodoElement from "./components/TodoElement";
+import {Header, Button, Icon} from 'react-native-elements';
+import { Dimensions } from "react-native";
+
+let width = Dimensions.get('window').width;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header
+        centerComponent={{ text: "To do list!", style: {color: 'white'} }}
+        innerContainerStyles={styles.header}
+        />
+        <ScrollView style={styles.todoList}>
+          <KeyboardAvoidingView style={{ flex: 1 }}
+                  keyboardVerticalOffset={100} behavior={"position"}>
+            
+            <TodoElement text="Finish my class" finished={true}/>
+            <TodoElement text="Finish another thing" finished={false}/>
+            <TodoElement text="woooo" finished={false}/>
+            
+            <View style={styles.input}>
+              <Icon name="note-add" />  
+              <TextInput 
+                style={styles.textInput}
+              />
+            </View>
+
+          </KeyboardAvoidingView>
+        </ScrollView>
+        
       </View>
     );
   }
@@ -19,5 +43,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: width
   },
+  header: {
+    width: width
+  },
+  todoList: {
+    width: width*0.9
+  },
+  input: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: width*0.8,
+    marginBottom: 10
+  }
 });
